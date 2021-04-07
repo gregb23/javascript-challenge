acd2175ff42e3b5cedd5328d405bba53aa486d9e
Gregory Brown <gregory.brown23@gmail.com>
2021-04-06 21:49:00 -0400

    working on app.js code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,18 @@
 // from data.js
-var tableData = data;
+const tableData = data;
 
 // YOUR CODE HERE!
+const button = d3.select ("#filter-btn")
+
+//select form
+const form = d3.select("#form")
+
+//select table
+const tableBody = d3.select(".table-body");
+
+//create event 
+button.on("click", runFilter);
+form.on("submit", runFilter);
+
+
+
